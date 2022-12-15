277583ea440e96c643db2025b525910f9f95b0c7
timurius <timur.yagudin.08@hmail.com>
2022-12-15 21:54:40 +0400

    Animation fixes and design a little

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ let titles = ["Order for Food", "Easy Payment", "Fast Delivery"];
 let texts = ["Order some food and be happy! Because it is yummy Order some food and be happy!", "Order some food and be happy! Because it is yummy Order some food and be happy!", "Order some food and be happy! Because it is yummy Order some food and be happy!"];
 let sliderProcess = false;
 let currentSlide = 0;
-
+let nextSlide = 0;
 function backToSlider(){
 	document.getElementById("content2").style.display = "block";
 	document.getElementById("content2__bottom-content").style.display = "block";
+	document.getElementById("content3").style.display = "none";
+	slider(1);
 }
 
 function slider(){
@@ -15,51 +17,27 @@ function slider(){
 		return;
 	}
 	sliderProcess = true;
-	let nextSlide;
-	
-	// else if(nextPage > 2){
-	// 	nextPage = 0;
-	// }
+
 	
 	let leftOfImage = parseInt(document.getElementById("slider__body").style.left);
 	if( arguments.length != 0 ){
 		nextSlide = arguments[0] - 1;
 	}
 	else{
-		nextSlide = (-1 * leftOfImage + 428) / 428;
+		nextSlide += 1;
 	}
 	if (nextSlide == 3) {
 		document.getElementById("content2").style.display = "none";
 		document.getElementById("content2__bottom-content").style.display = "none";
-		document.getElementById("slider__body").style.left = 0 + "px" 
 		document.getElementById("content3").style.display = "block";
+		sliderProcess = false;
 		return;
 	}
-	console.log(nextSlide);
-	let a = nextSlide - currentSlide;
-	console.log(a);
-	let distance = -428 * a;
-	let i = leftOfImage;
-	if(distance < 0){
-		let moving = setInterval( () => { 
-			if(i <= leftOfImage + distance){
-				sliderProcess = false;
-				clearInterval(moving);
-			}
-			document.getElementById("slider__body").style.left = i + "px" 
-			i += distance / 107;
-			
-		}, 1,)
-	}else if( distance > 0 ){
-		let moving = setInterval( () => { 
-			if(i >= leftOfImage + distance){
-				sliderProcess = false;
-				clearInterval(moving);
-			}
-			document.getElementById("slider__body").style.left = i + "px" 
-			i += distance / 107;
-			
-		}, 1,)
+	if(currentSlide < nextSlide){
+		document.getElementById("slider__body").style.left = leftOfImage + ( (currentSlide - nextSlide) * 428) + 'px';
+	}
+	else if( currentSlide > nextSlide){
+		document.getElementById("slider__body").style.left = leftOfImage + ( (currentSlide - nextSlide) * 428) + 'px';
 	}
 	
 	
@@ -70,4 +48,5 @@ function slider(){
 	}
 	document.getElementById("page-switcher-0" + (nextSlide + 1) ).className = "page-switcher__button_selected";
 	currentSlide = nextSlide;
+	sliderProcess = false;
 }
